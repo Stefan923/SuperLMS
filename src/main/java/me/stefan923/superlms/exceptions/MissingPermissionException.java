30969ecc137bd03e1e421d47dd20993e9539db6c
Stefan923 <stefanpopescu923@gmail.com>
2020-09-01 00:00:54 +0300

    Added new exception

--- src/main/java/me/stefan923/superlms/exceptions/MissingPermissionException.java
+++ src/main/java/me/stefan923/superlms/exceptions/MissingPermissionException.java
@@ -1,4 +1,11 @@
 package me.stefan923.superlms.exceptions;
 
 public class MissingPermissionException extends Exception {
+    public MissingPermissionException() {
+        super("");
+    }
+
+    public MissingPermissionException(final String string) {
+        super(string);
+    }
 }
